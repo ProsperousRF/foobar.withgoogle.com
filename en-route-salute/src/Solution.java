86bb493e2d5ba3fae078ebb934eb98ed9c9cbc99
Stanislav Rakitov <prosperousrf@gmail.com>
2021-10-04 13:35:45 +0300

    En Route Salute. Done

--- en-route-salute/src/Solution.java
+++ en-route-salute/src/Solution.java
@@ -1,8 +1,15 @@
-/**
- * @author Stanislav Rakitov
- */
+/** @author Stanislav Rakitov */
 public class Solution {
   public static int solution(String s) {
-    return 0;
+    int counter = 0;
+    if (s != null) {
+      int index;
+      while ((index = s.indexOf('>')) != -1) {
+        s = s.substring(index + 1);
+        int count = (int) s.chars().filter(ch -> ch == '<').count();
+        counter += count * 2;
+      }
+    }
+    return counter;
   }
 }
--- en-route-salute/src/SolutionTest.java
+++ en-route-salute/src/SolutionTest.java
@@ -1,11 +1,13 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /** @author Stanislav Rakitov */
 class SolutionTest {
 
   @Test
+  @DisplayName("<<>><")
   void solution4() {
     String testcase = "<<>><";
     int expected = 4;
@@ -14,6 +16,7 @@ class SolutionTest {
   }
 
   @Test
+  @DisplayName(">----<")
   void solution2() {
     String testcase = ">----<";
     int expected = 2;
@@ -21,4 +24,18 @@ class SolutionTest {
     assertEquals(expected, Solution.solution(testcase));
   }
 
+  @Test
+  @DisplayName("Empty string")
+  void empty() {
+    String testcase = "";
+    int expected = 0;
+    assertEquals(expected, Solution.solution(testcase));
+  }
+
+  @Test
+  @DisplayName("Null string")
+  void nullString() {
+    int expected = 0;
+    assertEquals(expected, Solution.solution(null));
+  }
 }
