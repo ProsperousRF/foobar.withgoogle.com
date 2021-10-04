80fa1f29c38f4d680bf1d9eee58dcc93d2e788aa
Stanislav Rakitov <prosperousrf@gmail.com>
2021-10-04 16:24:51 +0300

    "Doomsday Fuel" test cases

---
+++ doomsday-fuel/src/SolutionTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/** @author Stanislav Rakitov */
+class SolutionTest {
+
+  @Test
+  void solution1() {
+    int[][] testcase = {
+      {0, 2, 1, 0, 0},
+      {0, 0, 0, 3, 4},
+      {0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0}
+    };
+    int[] expected = {7, 6, 8, 21};
+
+    assertEquals(expected, Solution.solution(testcase));
+  }
+
+  @Test
+  void solution2() {
+    int[][] testcase = {
+      {0, 1, 0, 0, 0, 1},
+      {4, 0, 0, 3, 2, 0},
+      {0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0}
+    };
+    int[] expected = {0, 3, 2, 9, 14};
+
+    assertEquals(expected, Solution.solution(testcase));
+  }
+}
